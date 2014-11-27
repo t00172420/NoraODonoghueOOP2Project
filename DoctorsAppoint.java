b2f17c953d0fe10ff77f0c8efb1e4888f3e74d8b
unknown <t00172420@1XXB8CA3A8A70ED.students.ittralee.local>
2014-11-27 12:35:56 +0000

    Edited 27-11-2014

--- DoctorsAppoint.java
+++ DoctorsAppoint.java
@@ -1,14 +1,16 @@
 //DoctorsAppoint.java
 /*This program allows a user to choose a day from an array list and enter details into the array
  *to book a Doctors Appointment and return the details entered back to the user*/
- import javax.swing.*;
+
+import javax.swing.*;
 
 public class DoctorsAppoint {
 	public static void main (String args[]){
-		String [] day = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
 		String pName;
 		int pNumber;
 		
+		String [] day = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
 		JList dayList = new JList(day);
 	    JOptionPane.showMessageDialog(null,dayList,"Choose your day", JOptionPane.PLAIN_MESSAGE);
 	   
@@ -18,10 +20,14 @@ public class DoctorsAppoint {
 		JList timeList = new JList(time);
 	    JOptionPane.showMessageDialog(null,timeList,"Choose your time", JOptionPane.PLAIN_MESSAGE);
 	    
+	    String [] dName = {" Dr.Anita McCarthy "," Dr.Simon Jones "," Dr.Calvin O'Connor "," Dr.Maria Murphy "};
+	    JList dNameList = new JList(dName);
+	    JOptionPane.showMessageDialog(null,dNameList,"Choose your doctor", JOptionPane.PLAIN_MESSAGE);
+	    
 	    pName = JOptionPane.showInputDialog("Please enter your name: ");
 	    pNumber = Integer.parseInt(JOptionPane.showInputDialog("Please enter your contact number: "));
 	    int timeNum = timeList.getSelectedIndex();
 	    	     
-	    JOptionPane.showMessageDialog(null,"Book Appointment for " + dayList.getSelectedValue() + " at " + timeList.getSelectedValue());	
+	    JOptionPane.showMessageDialog(null,"Book Appointment for " + dayList.getSelectedValue() + " at " + timeList.getSelectedValue() + " with " + dNameList.getSelectedValue());	
 	}
 }
